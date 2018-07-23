db08d0a06533b6c536f63705b1585a72b4ae3afb
Jake Morrison <jake@cogini.com>
2018-07-23 10:52:49 +0000

    Debug nerves_config_ec2

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,9 @@
 # is restricted to this project.
 use Mix.Config
 
+# # Add the RingLogger backend. This removes the default :console backend.
+# config :logger, backends: [RingLogger]
+
 # Customize non-Elixir parts of the firmware.  See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
@@ -13,13 +16,16 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # docs for separating out critical OTP applications such as those
 # involved with firmware updates.
 config :shoehorn,
-  init: [:nerves_runtime, :nerves_config_ec2, :nerves_init_gadget],
+  init: [:nerves_runtime, :nerves_init_gadget, :nerves_config_ec2],
   app: Mix.Project.config()[:app]
+# config :shoehorn,
+#   init: [:nerves_runtime, :nerves_init_gadget],
+#   app: Mix.Project.config()[:app]
 
-# config :nerves_firmware_ssh,
-#   authorized_keys: [
-#     File.read!(Path.join(System.user_home!, ".ssh/authorized_keys"))
-#   ]
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(Path.join(System.user_home!, ".ssh/authorized_keys"))
+  ]
 
 config :nerves_init_gadget,
   ifname: "eth0",
