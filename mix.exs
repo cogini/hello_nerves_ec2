dac902d8c42231f41ba7fc9b488cdaf584a82889
Jake Morrison <jake@cogini.com>
2018-08-03 01:37:20 +0000

    Clean up

--- mix.exs
+++ mix.exs
@@ -49,9 +49,7 @@ defmodule HelloNervesEc2.MixProject do
     [
       {:nerves_runtime, "~> 0.4"},
       {:nerves_init_ec2, path: "../nerves_init_ec2"}
-      # {:nerves_init_gadget, "~> 0.3"},
-      # {:nerves_config_ec2, github: "cogini/nerves_config_ec2"},
-      # {:nerves_config_ec2, path: "../nerves_config_ec2"}
+      # {:nerves_init_ec2, github: "cogini/nerves_init_ec2"},
     ] ++ system(target)
   end
 
