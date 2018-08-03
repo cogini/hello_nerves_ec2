eb5c028d401757265a234c5b20c72dd43362379f
Jake Morrison <jake@cogini.com>
2018-08-03 01:38:37 +0000

    Clean up

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 use Mix.Config
 
-# # Add the RingLogger backend. This removes the default :console backend.
+# Add the RingLogger backend. This removes the default :console backend.
 # config :logger, backends: [RingLogger]
 
 # Customize non-Elixir parts of the firmware.  See
@@ -16,25 +16,27 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # docs for separating out critical OTP applications such as those
 # involved with firmware updates.
 config :shoehorn,
-  init: [:nerves_runtime, :nerves_init_gadget, :nerves_config_ec2],
+  init: [:nerves_runtime, :nerves_init_ec2],
   app: Mix.Project.config()[:app]
-# config :shoehorn,
-#   init: [:nerves_runtime, :nerves_init_gadget],
-#   app: Mix.Project.config()[:app]
+
+# config :nerves_network, :default,
+#   eth0: [
+#     ipv4_address_method: :dhcp
+#   ]
 
 config :nerves_firmware_ssh,
   authorized_keys: [
     File.read!(Path.join(System.user_home!, ".ssh/authorized_keys"))
   ]
 
-config :nerves_init_gadget,
+config :nerves_init_ec2,
   ifname: "eth0",
   address_method: :dhcp,
-  mdns_domain: nil,
-  node_name: nil,
-  # node_name: "hello_nerves_ec2",
   node_host: :ip,
-  ssh_console_port: 22
+  ssh_console_port: 22,
+  ssh_authorized_keys: [
+    # File.read!(Path.join(System.user_home!, ".ssh/authorized_keys"))
+  ]
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
